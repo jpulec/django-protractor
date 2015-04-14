b8d134e197cc5f8fe355d72f904fa5543163bd45
James Pulec <jpulec@gmail.com>
2015-04-14 15:33:34 -0700

    Version bump

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,3 +1,10 @@
+0.4
+===
+
+* Create a mixin to use with tests. This allows more easy creation of test
+  cases and easy setup of initial data via fixtures or factories.
+
+
 0.3
 ===
 
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-protractor',
-    version='0.3',
+    version='0.4',
     packages=find_packages(),
     include_package_data=True,
     install_requires=['django>=1.4'],
